419a1ece6aa20633ada8e8fbcb35a864d924689e
tavo <gasb_150@hotmail.com>
2021-03-29 18:04:16 -0500

    Remove guess user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
 
-    def current_user
-        if session[:user_id]
-        @current_user ||= User.find(session[:user_id])
-        else 
-            
-            @current_user ||= User.find(1)
-        end
-      end
+ def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+
 end
